8a6cf0231b067dd1186be33805cbbddf8d031743
Fernando Machado <fernandomachadoweb@gmail.com>
2022-09-17 21:26:25 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nomeOrigial = "Fernando Machado";
+        saudacao(nomeOrigial);
+    }
+
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
+    }
+
+}
